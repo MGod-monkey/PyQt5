70104e00b2a9c116aa1a9684d57463ef662c3b2a
MGod-monkey <wpqds666@163.com>
2020-10-17 13:38:44 +0800

    set StatusBox

--- useClipBoard.py
+++ useClipBoard.py
@@ -5,7 +5,7 @@ import sys
 
 # 剪切板使用案例
 
-class ClipBoard(QWidget):
+class ClipBoard(QMainWindow):
     def __init__(self):
         super(ClipBoard, self).__init__()
         self.setWindowTitle('使用剪切板')
@@ -30,6 +30,7 @@ class ClipBoard(QWidget):
         self.textPasteButton.clicked.connect(self.pasteText)
         self.imageCopyButton.clicked.connect(self.copyimage)
         self.imagePasteButton.clicked.connect(self.pasteimage)
+        widget = QWidget()
         Glayout = QGridLayout()
         Glayout.addWidget(self.lineEdit,0,0,1,3)
         Glayout.addWidget(self.textCopyButton,0,3,1,1)
@@ -37,32 +38,37 @@ class ClipBoard(QWidget):
         Glayout.addWidget(self.imagelabel,1,0,2,3)
         Glayout.addWidget(self.imageCopyButton,1,3,1,2)
         Glayout.addWidget(self.imagePasteButton,2,3,1,2)
-        self.setLayout(Glayout)
+        widget.setLayout(Glayout)
+        self.setCentralWidget(widget)
+
     def copyText(self):
         # 剪切板
         clipboard = QApplication.clipboard()
         # 将编辑框的文字复制到剪切板中
         clipboard.setText(self.lineEdit.text())
-        ok = QMessageBox.information(self,'消息框','复制成功',QMessageBox.Ok)
+        # ok = QMessageBox.information(self,'消息框','复制成功',QMessageBox.Ok)
+        status = self.statusBar()
+        status.showMessage('复制成功',1000)
     def pasteText(self):
         clipboard = QApplication.clipboard()
         # 将裁剪版的内容粘贴到编辑框中
         self.lineEdit.setText(clipboard.text())
-        # status = QStatusBar(self)
-        # status.showMessage('粘贴成功', 1000)
-        ok = QMessageBox.information(self,'消息框','粘贴成功',QMessageBox.Ok)
+        status = self.statusBar()
+        status.showMessage('粘贴成功', 1000)
+        # ok = QMessageBox.information(self,'消息框','粘贴成功',QMessageBox.Ok)
     def copyimage(self):
         clipboard = QApplication.clipboard()
         # 将图片内容复制到裁剪版
         clipboard.setPixmap(self.imagelabel.pixmap())
         clipboard.setText(self.lineEdit.text())
-        ok = QMessageBox.information(self, '消息框', '复制成功', QMessageBox.Ok)
+        status = self.statusBar()
+        status.showMessage('复制成功', 1000)
     def pasteimage(self):
         clipboard = QApplication.clipboard()
         # 将裁剪版的图片复制到图片标签
         self.imagelabel.setPixmap(clipboard.pixmap())
-        # status = QStatusBar()
-        # status.showMessage('粘贴成功', 1000)
+        status = self.statusBar()
+        status.showMessage('粘贴成功', 1000)
 
 
 
